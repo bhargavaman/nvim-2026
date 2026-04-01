12567f4aa43a06872a5e654f9d40fe23cf2c56cb
Aman Bhargava <amanbhargava316@gmail.com>
2026-04-01 07:49:32 +0530

    whichkey and webdev icons

--- lua/aman/plugins.lua
+++ lua/aman/plugins.lua
@@ -70,6 +70,7 @@ require("supermaven-nvim").setup({
 --: WhichKey
 vim.pack.add({
   { src = "https://github.com/folke/which-key.nvim" },
+  { src = "https://github.com/nvim-tree/nvim-web-devicons" },
 })
 local wk = require("which-key")
 wk.add({
@@ -77,5 +78,47 @@ wk.add({
 	{ "<leader>y", '"+y', desc = "Yank to clipboard" },
 	{ "<leader>Y", '"+Y', desc = "Yank line to clipboard" },
 	{ "<leader>p", '"+p', desc = "Paste from clipboard" },
+})
+wk.add({
+	{ "<leader><tab>", group = "tabs" },
+	{ "<leader>c", group = "code" },
+	{ "<leader>d", group = "debug" },
+	{ "<leader>D", group = "Diffview", icon = { icon = "", color = "orange" } },
+	{ "<leader>dp", group = "profiler" },
+	{ "<leader>f", group = "find" },
+	{ "<leader>g", group = "git" },
+	{ "<leader>gh", group = "hunks" },
+	{ "<leader>q", group = "quit/session" },
+	{ "<leader>s", group = "surround" },
+	{ "<leader>u", group = "ui", icon = { icon = "󰙵 ", color = "cyan" } },
+	{ "<leader>x", group = "diagnostics/quickfix", icon = { icon = "󱖫 ", color = "green" } },
+	{ "[", group = "prev" },
+	{ "]", group = "next" },
+	{ "g", group = "goto" },
+	{ "gs", group = "surround" },
+	{ "z", group = "fold" },
+	{
+		"<leader>b",
+		group = "buffer",
+		expand = function()
+			return require("which-key.extras").expand.buf()
+		end,
+	},
+	-- better descriptions
+	{ "gx", desc = "Open with system app" },
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Buffer Keymaps (which-key)",
+		},
+		{
+			"<c-w><space>",
+			function()
+				require("which-key").show({ keys = "<c-w>", loop = true })
+			end,
+			desc = "Window Hydra Mode (which-key)",
+	},
 })
 --:
