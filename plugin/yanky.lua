c1893ccfb4a644c45ef6c4528edc070a68636305
Aman Bhargava <amanbhargava316@gmail.com>
2026-04-03 19:49:45 +0530

    autopairs and better yanky

--- plugin/yanky.lua
+++ plugin/yanky.lua
@@ -2,4 +2,10 @@ vim.pack.add({
   { src = "https://github.com/gbprod/yanky.nvim" },
 })
 
-require("yanky").setup({})
+require("yanky").setup({
+  highlight = {
+    on_put = false,
+    on_yank = true,
+    timer = 100,
+  },
+})
