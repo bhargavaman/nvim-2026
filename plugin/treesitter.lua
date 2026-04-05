842e6ed72a0ecf75aec90b62ca6ed2d185ad100e
Aman Bhargava <amanbhargava316@gmail.com>
2026-04-06 02:54:43 +0530

    mini adds

--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -2,6 +2,7 @@ vim.pack.add({
   {
     src = "https://github.com/nvim-treesitter/nvim-treesitter",
     version = "main",
+    build = ":TSUpdate",
   },
   {
     src = "https://github.com/nvim-treesitter/nvim-treesitter-textobjects",
