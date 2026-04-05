ef4374609a2db0515d0e369e17efd79915a4e83d
Aman Bhargava <amanbhargava316@gmail.com>
2026-04-06 03:33:30 +0530

    theme takes colors from terminal now

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -1,1 +1,24 @@
 vim.cmd([[colorscheme catppuccin]])
+
+local function set_transparent() -- set UI component to transparent
+  local groups = {
+    "Normal",
+    "NormalNC",
+    "EndOfBuffer",
+    "NormalFloat",
+    "FloatBorder",
+    "SignColumn",
+    "StatusLine",
+    "StatusLineNC",
+    "TabLine",
+    "TabLineFill",
+    "TabLineSel",
+    "ColorColumn",
+  }
+  for _, g in ipairs(groups) do
+    vim.api.nvim_set_hl(0, g, { bg = "none" })
+  end
+  vim.api.nvim_set_hl(0, "TabLineFill", { bg = "none", fg = "#767676" })
+end
+
+set_transparent()
