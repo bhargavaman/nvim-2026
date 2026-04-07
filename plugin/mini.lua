f6c9cd035dd0dc3b47ccbf5e8f53c0adbe117255
Aman Bhargava <amanbhargava316@gmail.com>
2026-04-07 07:24:12 +0530

    surround keymaps

--- plugin/mini.lua
+++ plugin/mini.lua
@@ -5,12 +5,12 @@ vim.pack.add({
 require("mini.surround").setup({
   -- Module mappings. Use `''` (empty string) to disable one.
   mappings = {
-    add = "<leader>msa", -- Add surrounding in Normal and Visual modes
-    delete = "<leader>msd", -- Delete surrounding
-    find = "<leader>msf", -- Find surrounding (to the right)
-    find_left = "<leader>msF", -- Find surrounding (to the left)
-    highlight = "<leader>m", -- Highlight surrounding
-    replace = "<leader>msr", -- Replace surrounding
+    add = "<leader>as", -- Add surrounding in Normal and Visual modes
+    delete = "<leader>ds", -- Delete surrounding
+    find = "", -- Find surrounding (to the right)
+    find_left = "", -- Find surrounding (to the left)
+    highlight = "", -- Highlight surrounding
+    replace = "<leader>cs", -- Replace surrounding
     suffix_last = "", -- Suffix to search with "prev" method
     suffix_next = "", -- Suffix to search with "next" method
   },
