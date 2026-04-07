0addb4ce5d55f42f2c1ee7785c534e8f2a95ce90
Aman Bhargava <amanbhargava316@gmail.com>
2026-04-07 07:19:28 +0530

    ensure installed

--- plugin/mason.lua
+++ plugin/mason.lua
@@ -6,6 +6,33 @@ vim.pack.add({
 })
 require("mason").setup({})
 require("mason-lspconfig").setup({
-  ensure_installed = {},
+  ensure_installed = {
+    "vue_ls",
+    "vtsls",
+    "tailwindcss",
+    "hyprls",
+    "biome",
+    "astro",
+    "bashls",
+    "elixirls",
+    "fish_lsp",
+    "gh_actions_ls",
+    "glsl_analyzer",
+    "gopls",
+    "html",
+    "jqls",
+    "jsonls",
+    "lua_ls",
+    "ols",
+    "pico8_ls",
+    "rust_analyzer",
+    "stylua",
+    "systemd_lsp",
+    "templ",
+    "termux_language_server",
+    "yamlls",
+    "zls",
+    "wasm_language_tools",
+  },
 })
 --:
