98e55775b25098f0af70f0d48e0b5bf60d980b9b
Aman Bhargava <amanbhargava316@gmail.com>
2026-04-12 06:35:16 +0530

    theme

--- plugin/themes.lua
+++ plugin/themes.lua
@@ -27,25 +27,25 @@ require("cyberdream").setup({
   variant = "default", -- use "light" for the light variant. Also accepts "auto" to set dark or light colors based on the current value of `vim.o.background`
 
   -- Enable transparent background
-  transparent = false,
+  transparent = true,
 
   -- Reduce the overall saturation of colours for a more muted look
   saturation = 1, -- accepts a value between 0 and 1. 0 will be fully desaturated (greyscale) and 1 will be the full color (default)
 
   -- Enable italics comments
-  italic_comments = false,
+  italic_comments = true,
 
   -- Replace all fillchars with ' ' for the ultimate clean look
-  hide_fillchars = false,
+  hide_fillchars = true,
 
   -- Apply a modern borderless look to pickers like Telescope, Snacks Picker & Fzf-Lua
-  borderless_pickers = false,
+  borderless_pickers = true,
 
   -- Set terminal colors used in `:terminal`
   terminal_colors = true,
 
   -- Improve start up time by caching highlights. Generate cache with :CyberdreamBuildCache and clear with :CyberdreamClearCache
-  cache = false,
+  cache = true,
 
   -- Override highlight groups with your own colour values
   highlights = {
@@ -88,10 +88,14 @@ require("cyberdream").setup({
 
   -- Disable or enable colorscheme extensions
   extensions = {
-    telescope = true,
     notify = true,
     mini = true,
-    ...,
+    blinkcmp = true,
+    dapui = true,
+    gitsigns = true,
+    helpview = true,
+    snacks = true,
+    whichkey = true,
   },
 
   -- Alternatively, you can use 'default' to set all extensions at once
@@ -108,4 +112,4 @@ require("cyberdream").setup({
 })
 
 -- Set Theme
-vim.cmd([[colorscheme tokyodark]])
+vim.cmd([[colorscheme cyberdream]])
